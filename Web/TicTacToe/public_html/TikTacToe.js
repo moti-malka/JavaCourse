6fe90606b5ecc6390224e913fb19fbcea047563b
motim6026 <moti.malka25@gmail.com>
2018-01-15 00:25:35 +0200

    Signed-off-by: motim6026 <moti.malka25@gmail.com>

--- Web/TicTacToe/public_html/TikTacToe.js
+++ Web/TicTacToe/public_html/TikTacToe.js
@@ -7,4 +7,32 @@ and open the template in the editor.
     Created on : Jan 13, 2018, 9:44:04 PM
     Author     : Motim
 */
+var XO = "X"
+var ind = 1;
 
+function insertXO(td){
+    
+    if(ind === 1){
+        XO = "X";
+        ind--;
+    }
+    else{
+        XO = "O";
+        ind++;
+    }
+    td.innerHTML = XO;
+    CheckWinner();
+    
+}
+
+function CheckWinner(td){
+    var x = td.value;
+    alert(x);
+    
+}
+
+
+
+function restart(){
+    location.reload();
+}
